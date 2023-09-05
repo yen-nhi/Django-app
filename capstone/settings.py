88ce07423b4894524ab95276258b32c27f07ee29
yen-nhi <56079401+yen-nhi@users.noreply.github.com>
2023-09-05 16:57:45 +0700

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = json.loads(os.getenv('ALLOWED_HOSTS', '["localhost", "127.0.0.1"]'))
 
