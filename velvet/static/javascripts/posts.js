9462c1e4f7499e3cda914d3b2e788ba25819e6d0
nhi.nty <nhi.nty@teko.vn>
2023-10-04 01:39:50 +0700

    update

--- velvet/static/javascripts/posts.js
+++ velvet/static/javascripts/posts.js
@@ -29,7 +29,7 @@ function build_post(post){
     const card = document.createElement('div');
     card.className = 'card';
     card.innerHTML = `<div class="card-body">
-                        <h5 class="card-title"><a href="/post/${post.id}">${post.username}</a></h5>
+                        <h5 class="card-title"><a href="/post/${post.id}">${post.title}</a></h5>
                         <p class="card-text" class="bottom"><small>${post.time}</small></p>
                       </div>`;
     document.querySelector('#posts').append(card);
@@ -43,7 +43,7 @@ function build_article(post){
       <span><img class="avatar" src="${post.avatar}" alt="..." width="100%"><span
       <span style="font-weight: bold">${ post.username }</span>
     </div >
-      <div class="post-body">${ post.body.slice(0,200) }...</div>
+    <div class="post-body">${ post.body.slice(0,200) }...</div>
   </a>`;
   document.querySelector('#articles').append(card);
 }
