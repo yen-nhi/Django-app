eb0f87d8147b640eb689269cb2d4951d8324f07f
nhi.nty <nhi.nty@teko.vn>
2023-10-04 19:44:07 +0700

    update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -148,8 +148,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'velvet/static/'),)
+STATIC_ROOT = 'static/'
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'velvet/static/'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
